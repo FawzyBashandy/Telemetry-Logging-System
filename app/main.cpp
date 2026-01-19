4e94223f8fe3fd1023e1151f3b54d15693dc0089
FawzyBashandy <fawzybashandy@gmail.com>
2026-01-19 02:04:15 +0200

    Phase2_Trial

--- app/main.cpp
+++ app/main.cpp
@@ -1,12 +1,20 @@
 #include "LogManager.hpp"
 #include "LogMessage.hpp"
-
+#include "FileTelemetrySource.hpp"
 #include "ConsoleSink.hpp"
 #include "FileSink.hpp"
 
 #include <memory>
-
+void phase1_Trial();
+void phase2_Trial();
 int main()
+{
+
+    //phase1_Trial();
+    phase2_Trial();
+    return 0;
+}
+void phase1_Trial()
 {
     LogManager logManager;
 
@@ -43,6 +51,35 @@ int main()
 
     // Route buffered messages to all sinks
     logManager.flush();
+}
+void phase2_Trial()
+{
+    LogManager logManager;
+    FileTelemetrySource statFile("/proc/stat");
+    FileTelemetrySource meminfoFile("/proc/meminfo");
+    std::string data;
+    //Open Sources
+    statFile.openSource();
+    meminfoFile.openSource();
 
-    return 0;
+    // Add sinks
+    logManager.addSink(
+        std::make_unique<ConsoleSink>(ConsoleSink::Stream::StdOut)
+    );
+
+    //Read And log Sources
+    statFile.readSource(data);
+    logManager.pushMessage(LogMessage{
+                                            std::string("/proc/stat"),
+                                            data,
+                                            eLogSeverity::Info,
+                                            std::string("Trial")});
+    
+    meminfoFile.readSource(data);
+    logManager.pushMessage(LogMessage{
+                                            std::string("/proc/meminfo"),
+                                            data,
+                                            eLogSeverity::Info,
+                                            std::string("Trial")});
+    logManager.flush();
 }
